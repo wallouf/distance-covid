b4deb55c2f56fff1633376da94275a73012d378c
wallouf <ct13860@gmail.com>
2020-05-04 15:22:50 +0200

    Move to openAPIs

--- js/covid-locator.js
+++ js/covid-locator.js
@@ -7,6 +7,7 @@ SudokuSolver.map = SudokuSolver.map || {};
 var initLat = 48.852969;
 var initLon = 2.349903;
 var map = null;
+var locationMarker = null;
 var positionCircle = null;
 
 var payloadToken = {};
@@ -23,122 +24,61 @@ var addressFound = false;
         }
     }
 
-    function autocompleteAddress(){
-        var input = document.getElementById('locatorAddress');
-        var addressOptions = {
-            componentRestrictions: {
-                country: 'fr'
+    function autoCompleteAddress(){
+        $("#locatorAddress").on( "keydown", function( event ) {
+            if ( event.keyCode === $.ui.keyCode.TAB &&
+                $( this ).autocomplete( "instance" ).menu.active ) {
+              event.preventDefault();
             }
-        };
-        
-        var autocomplete = new google.maps.places.Autocomplete(input, addressOptions);
-
-        $("#locatorBtn").click(function() {
-            displayAddressAlert();
-            var text = $("#locatorAddress").text();
-            var place = autocomplete.getPlace();
-            
-            if(place != undefined){
-                var lat = place.geometry.location.lat();
-                var long = place.geometry.location.lng();
-
-                var myLatLng = {lat: lat, lng: long};
-
-                map = new google.maps.Map(document.getElementById('locatorMaps'), {
-                    zoom: 8,
-                    center: myLatLng
-                });
-
-                var marker = new google.maps.Marker({
-                    position: myLatLng,
-                    map: map,
-                    title: 'Home'
-                });
-
-                var homeAllowMvtCircle = new google.maps.Circle({
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 0.7,
-                    strokeWeight: 2,
-                    fillColor: '#FF0000',
-                    fillOpacity: 0.35,
-                    map: map,
-                    center: myLatLng,
-                    radius: 100 * 1000
-                });
-                addressFound = true;
-
-                
-            } else if(text == undefined || text.trim() == ""){
-                displayAddressAlert("Inscrivez votre adresse dans le champ prévu a cet effet.");
-                addressFound = false;
-            } else {
-                displayAddressAlert("Utilisez les choix proposes pour valider la saisie.");
-                addressFound = false;
-            }
-        });
-    }
-
-    function displayLocation(position){
-        if(positionCircle != undefined){
-            positionCircle.setMap(null);
-        }
-
-        positionCircle = new google.maps.Circle({
-            strokeColor: '#0000FF',
-            strokeOpacity: 1,
-            strokeWeight: 2,
-            fillColor: '#0000FF',
-            fillOpacity: 0.35,
-            map: map,
-            center: position,
-            radius: 1000
-        });
-        map.setCenter(position);
-    }
-
-    function initMySimpleLocationBtn(){
-        $("#myLocationBtn").click(function() {
-
-            if(addressFound == false){
-                displayAddressAlert("Trouvez d'abord votre adresse.");
-            }else{
-                $.getJSON("http://ip-api.com/json", function (data, status) {
-                    if(status === "success") {
-                        if(data.lat && data.lon) {
-                            //if there's not zip code but we have a latitude and longitude, let's use them
-                            var pos = {
-                                lat: data.lat,
-                                lng: data.lon
-                            };
-
-                            displayLocation(pos);
-                        } else {
-                            //if there's an error 
-                            handleLocationError(true);
-                        }
-                    } else {
-                        handleLocationError(false);
+        })
+        .autocomplete({
+            minLength: 2,
+            source: function (request, response) {
+                $.ajax({
+                    url: "https://api-adresse.data.gouv.fr/search/",
+                    data: { 
+                      q: request.term,
+                      limit: 5
+                    },
+                    dataType: "json",
+                    success: function (data) {
+                        response($.map(data.features, function (item) {
+                            return { label: item.properties.label, value: item.properties.label, coordinates: item.geometry.coordinates};
+                        }));
                     }
                 });
+            },
+            select: function ( event, ui) {
+                // $("#locatorAddress").val(ui.item.label);
+
+
+                alert('You selected: ' + ui.item.coordinates);
             }
         });
     }
 
-    function initMyGoogleLocationBtn(){
-        $("#myLocationBtn").click(function() {
+    function searchAgain(){
 
+    }
+
+    function displayResult(){
+
+    }
+
+    function initMyLocationBtn(){
+        $("#myLocationBtn").click(function() {
             if(addressFound == false){
                 displayAddressAlert("Trouvez d'abord votre adresse.");
             }else{
                 // Try HTML5 geolocation.
                 if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(function(position) {
-                        var pos = {
-                            lat: position.coords.latitude,
-                            lng: position.coords.longitude
-                        };
-
-                        displayLocation(pos);
+                    navigator.geolocation.getCurrentPosition(function(location) {
+                        if (locationMarker != undefined){
+                            map.removeLayer(locationMarker);
+                        }
+                        var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+                        locationMarker = L.marker(latlng).addTo(map).bindPopup('Domicile');
+                        map.setView(latlng, 7);
 
                     }, function() {
                         handleLocationError(true);
@@ -153,7 +93,6 @@ var addressFound = false;
 
 
     function saveBase64AsFile(base64, fileName) {
-
         var link = document.createElement("a");
         link.setAttribute("href", base64);
         link.setAttribute("download", fileName);
@@ -182,29 +121,14 @@ var addressFound = false;
 
     // Fonction d'initialisation de la carte
     function initMap() {
-        // Créer l'objet "map" et l'insèrer dans l'élément HTML qui a l'ID "map"
-        map = new google.maps.Map(document.getElementById("locatorMaps"), {
-            // Nous plaçons le centre de la carte avec les coordonnées ci-dessus
-            center: new google.maps.LatLng(initLat, initLon), 
-            // Nous définissons le zoom par défaut
-            zoom: 11, 
-            // Nous définissons le type de carte (ici carte routière)
-            mapTypeId: google.maps.MapTypeId.ROADMAP, 
-            // Nous activons les options de contrôle de la carte (plan, satellite...)
-            mapTypeControl: false,
-            // Nous désactivons la roulette de souris
-            scrollwheel: true, 
-            mapTypeControlOptions: {
-                // Cette option sert à définir comment les options se placent
-                style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR 
-            },
-            // Activation des options de navigation dans la carte (zoom...)
-            navigationControl: true, 
-            navigationControlOptions: {
-                // Comment ces options doivent-elles s'afficher
-                style: google.maps.NavigationControlStyle.ZOOM_PAN 
-            }
+        map = L.map('locatorMaps').setView([initLat, initLon], 10); // LIGNE 18
+
+        var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { // LIGNE 20
+            attribution: '© OpenStreetMap contributors',
+            maxZoom: 19
         });
+    
+        map.addLayer(osmLayer);
     }
 
     // Register click handler for #request button
@@ -212,8 +136,8 @@ var addressFound = false;
         addressFound = false;
 
         initMap();
-        autocompleteAddress();
-        initMyGoogleLocationBtn();
+        autoCompleteAddress();
+        initMyLocationBtn();
         initSaveBtn();
 
     });
